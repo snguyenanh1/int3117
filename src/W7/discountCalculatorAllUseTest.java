aed4b9e3f0d7915c4cde5423a4f57f563cb13348
snguyenanh1 <snguyenanh1@gmail.com>
2025-10-26 11:51:34 +0700

    W7

--- src/W7/discountCalculatorAllUseTest.java
+++ src/W7/discountCalculatorAllUseTest.java
@@ -1,4 +1,4 @@
-package W5;
+package W7;
 
 import W3.CustomerTier;
 import W3.NegativeTotalException;
@@ -8,18 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class discountCalculatorC2Test {
+
+public class discountCalculatorAllUseTest {
     @ParameterizedTest
-    @CsvFileSource(resources = "validtestcase.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "testcase.csv", numLinesToSkip = 1)
     void testReturnDiscount_ValidOrderTotal (int id, CustomerTier customerTier, int orderTotal, int expectedDiscount) throws NegativeTotalException {
         int actualDiscount = discountCalculator.returnDiscount(customerTier, orderTotal);
         Assertions.assertEquals(expectedDiscount, actualDiscount);
-    }
-
-    @Test
-    void testReturnDiscount_throwsException () {
-        Assertions.assertThrows(NegativeTotalException.class, () -> {
-            discountCalculator.returnDiscount(CustomerTier.COPPER, -100);
-        });
     }
 }
